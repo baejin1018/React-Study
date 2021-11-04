1b8f55a30776ae4b0f579fd77a93418fb51dc664
Bae-Jin-Young <bae20051018@gmail.com>
2021-11-04 22:16:00 +0900

    Study : zeroChoWebPackBuild

--- zeroChoReact/2 챕터/웹팩/WordRelay.jsx
+++ zeroChoReact/2 챕터/웹팩/WordRelay.jsx
@@ -2,9 +2,13 @@ const React = require("react");
 const { Component } = React;
 
 class WordRelay extends Component {
-  state = {};
+  state = {
+    text: "Hello webpack",
+  };
 
-  render() {}
+  render() {
+    return <h1>{this.state.text}</h1>;
+  }
 }
 
 module.exports = WordRelay; // 쪼갠 파일에서 쓰는 컴포넌트를 밖에써도 쓰게 해준느것
--- zeroChoReact/2 챕터/웹팩/webpack.config.js
+++ zeroChoReact/2 챕터/웹팩/webpack.config.js
@@ -11,9 +11,22 @@ module.exports = {
     //입력
     app: ["./client"],
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, //js 파일과jsx파일에 이 룰을 적용
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+
   output: {
     //출력
-    path: path.join(__dirname, "끝말잇기"),
+    path: path.join(__dirname, "wordRelay"),
     filename: "app.js",
   },
 };
