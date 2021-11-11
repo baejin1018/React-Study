9784231d3f8faf9c7e93950e35164975b7f0024a
Bae-Jin-Young <bae20051018@gmail.com>
2021-11-11 20:13:27 +0900

    Study : hooksBaseballGame

--- zeroChoReact/3챕터/Try.jsx
+++ zeroChoReact/3챕터/Try.jsx
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 
 class Try extends Component {
   render() {
+    /*const {tryInfo} = this.props; 이걸쓰면 밑에 주석처럼 this.props를 생략가능하다*/
     return (
       <li>
         <div>{this.props.tryInfo.try}</div>
+        {/* <div>{tryInfo.try}</div> */}
         <div>{this.props.tryInfo.result}</div>
+        {/* <div>{tryInfo.result}</div> */}
       </li>
     );
   }
