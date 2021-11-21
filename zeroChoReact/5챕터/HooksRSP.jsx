5be71ac0842e3f4e05341d0698e18fb3e384547e
Bae-Jin-Young <bae20051018@gmail.com>
2021-11-21 20:30:18 +0900

    Study : zerochoRSPGame(Hooks)

---
+++ zeroChoReact/5챕터/HooksRSP.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const rspCoords = {
+  바위: "0",
+  가위: "-142px",
+  보: "-284px",
+};
+
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
+
+const computerChoice = (imgCoord) => {
+  return Object.entries(rspCoords).find(function (v) {
+    return v[1] === imgCoord;
+  })[0];
+};
+
+const HooksRSP = () => {
+  const [result, setResult] = useState("");
+  const [imgCoord, setImgCoord] = useState(rspCoords.바위);
+  const [score, setScore] = useState(0);
+  const interval = useRef();
+
+  useEffect(() => {
+    //라이프 사이클 대체
+    //첫번쨰는 함수가 들어가고
+    //두번째 배열은  useEffect를 실행하고 싶은 state를 넣는다
+    //여기서는 imgCoord가 들어감
+    // componentDidMount, componentDidUpdate 역할(1대1 대응은 아님)
+    interval.current = setInterval(changeHand, 100);
+    return () => {
+      //componentWillUnmount 역할
+      clearInterval(interval.current);
+    };
+  }, [imgCoord]);
+
+  const changeHand = () => {
+    if (imgCoord === rspCoords.바위) {
+      setImgCoord(rspCoords.가위);
+    } else if (imgCoord === rspCoords.가위) {
+      setImgCoord(rspCoords.보);
+    } else if (imgCoord === rspCoords.보) {
+      setImgCoord(rspCoords.바위);
+    }
+  };
+
+  const onClickBtn = (choice) => () => {
+    clearInterval(interval.current);
+    const myScore = scores[choice];
+    const cpuScore = scores[computerChoice(imgCoord)];
+    const diff = myScore - cpuScore;
+    if (diff === 0) {
+      setResult("비겼습니다!");
+    } else if ([-1, 2].includes(diff)) {
+      setResult("이겼습니다");
+      setScore((prevScore) => prevScore + 1);
+    } else {
+      setResult("졌습니다");
+      setScore((prevScore) => prevScore - 1);
+    }
+    setTimeout(() => {
+      interval.current = setInterval(changeHand, 250);
+    }, 1000);
+  };
+
+  return (
+    <>
+      <h1>Hooks 가위 바위 보 !</h1>
+      <div
+        id="computer"
+        style={{
+          background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
+        }}
+      />
+      <div>
+        <button id="rock" className="btn" onClick={onClickBtn("바위")}>
+          바위
+        </button>
+        <button id="scissor" className="btn" onClick={onClickBtn("가위")}>
+          가위
+        </button>
+        <button id="paper" className="btn" onClick={onClickBtn("보")}>
+          보
+        </button>
+      </div>
+      <div>{result}</div>
+      <div>현재 {score}점</div>
+    </>
+  );
+};
+
+export default HooksRSP;
