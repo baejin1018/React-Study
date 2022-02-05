bcc68e2cf96f4e0a24cf5f94b6f76c8b918f04ad
Bae-Jin-Young <bae20051018@gmail.com>
2022-02-05 23:46:27 +0900

    particulates 만들기

--- toyProject/particulates/API.md
+++ toyProject/particulates/API.md
@@ -26,3 +26,15 @@ axios({
 - params (선택)
 
 이러한 정보를 같이 보내야한다
+
+# API KEY 숨기기
+
+### 최상위에 .env 라는 폴더를 만든다 (gitignore에 .env를 추가)
+
+### .env 폴더안에 `REACT_APP_ 이름 = APIKEY` 이런식으로 API key들을 적어준다
+
+### ex) REACT_APP_ABC_KEY=1234514tqrfadgvsfbs
+
+### jsx,js에서 사용할떄는 `process.env.REACT0_APP_이름` 이런식으로 사용한다
+
+### ex) const API_KEY = process.env.REACT_APP_ABC_KEY;
--- toyProject/particulates/src/App.js
+++ toyProject/particulates/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Particulates from "./Particulates";
+import axios from "axios";
+import { useEffect } from "react";
 function App() {
+  const API_KEY = process.env.REACT_APP_PARTICULATES_KEY;
+  const url = `https://cors-anywhere.herokuapp.com/http://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo?serviceKey=${API_KEY}&returnType=json&year=2022`;
+  axios.get(url).then((res) => console.log(res.data.response.body));
+
+  // const pinfo = async () => {
+  //   try {
+  //     const response = await axios.get(url);
+  //     console.log(url);
+  //     console.log(response);
+  //   } catch (error) {}
+  // };
+
+  // useEffect(() => {
+  //   pinfo();
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Particulates />
     </div>
   );
 }
