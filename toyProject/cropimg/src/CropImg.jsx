e981c11a6a17553b21c3dfc077f0a0e42d646913
Bae-Jin-Young <bae20051018@gmail.com>
2022-04-19 22:07:11 +0900

    Study : imgCrop

--- toyProject/cropimg/src/CropImg.jsx
+++ toyProject/cropimg/src/CropImg.jsx
@@ -1,42 +1,77 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useRef } from "react";
 import Cropper from "react-easy-crop";
+import "./CropImg.css";
 import defaultImg from "./logo192.png";
 const CropImg = () => {
+  const canvas = useRef();
   const [crop, setCrop] = useState({ x: 0, y: 0 });
-  const [zoom, setZoom] = useState(1);
   const [imgFile, setImgFile] = useState(defaultImg);
   const [choseFile, setChoseFile] = useState(false);
-
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState({});
+  const [dontShowDefault, setDontShowDefault] = useState(false);
+  const [dontShowCanvas, setDontShowCanvas] = useState(true);
   const saveFileImg = (e) => {
+    setDontShowDefault(true);
     setImgFile(URL.createObjectURL(e.target.files[0]));
     setChoseFile(true);
+    setDontShowCanvas(false);
   };
 
+  const onClickConfirm = useCallback(() => {
+    setChoseFile(false);
+  }, []);
+
   const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
-    console.log(croppedArea, croppedAreaPixels);
+    const ctx = canvas.current.getContext("2d");
+    const image = document.getElementById("source");
+
+    canvas.current.setAttribute("width", croppedAreaPixels.width);
+    canvas.current.setAttribute("height", croppedAreaPixels.height);
+    ctx.drawImage(
+      image,
+      croppedAreaPixels.x,
+      croppedAreaPixels.y,
+      croppedAreaPixels.width,
+      croppedAreaPixels.height,
+      0,
+      0,
+      croppedAreaPixels.width,
+      croppedAreaPixels.height
+    );
   }, []);
 
   return (
     <div>
-      <img src={imgFile} style={{ width: 200, height: 200 }} />
+      <img
+        id="source"
+        src={imgFile}
+        className={dontShowDefault ? "dontshow" : ""}
+        style={{ width: 200, height: 200 }}
+      />
+      <canvas
+        className={dontShowCanvas ? "dontshow" : ""}
+        id="source"
+        style={{ width: 200, height: 200 }}
+        ref={canvas}
+        width={croppedAreaPixels.width}
+        height={croppedAreaPixels.height}
+      ></canvas>
       <input type="file" onChange={saveFileImg} accept="image/*" />
       {choseFile && (
         <div style={{ position: "relative", width: 400, height: 400 }}>
           <Cropper
             image={imgFile}
             crop={crop}
-            zoom={zoom}
             aspect={1 / 1}
             onCropChange={setCrop}
             onCropComplete={onCropComplete}
-            onZoomChange={setZoom}
             cropShape={"round"}
             cropSize={{ width: 200, height: 200 }}
             showGrid={false}
           />
         </div>
       )}
-      <button>확인</button>
+      <button onClick={onClickConfirm}>확인</button>
     </div>
   );
 };
